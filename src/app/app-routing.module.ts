e7531fdb234c8cdfe4d10c6b9312c39ec24495d5
zankeii <42954203+zankeii@users.noreply.github.com>
2019-05-02 14:55:30 -0400

    Creacion de pages para hacer navegacion entre paginas
    utilizacion de routerlink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: 'inicio', loadChildren: './pages/inicio/inicio.module#InicioPageModule' },
+  { path: 'alert', loadChildren: './pages/alert/alert.module#AlertPageModule' },
+  { path: 'action-sheet', loadChildren: './pages/action-sheet/action-sheet.module#ActionSheetPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/pages/action-sheet/action-sheet.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ActionSheetPage } from './action-sheet.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ActionSheetPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [ActionSheetPage]
+})
+export class ActionSheetPageModule {}
